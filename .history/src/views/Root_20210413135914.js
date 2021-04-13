b1a1832ae0664d8dcca1d72bd1ad8b916dce7796
Michał Mroziński <mrozeksw@gmail.com>
2021-04-13 14:00:03 +0200

    correct error with context.Provider in Root.js

--- .history/src/views/Root_20210413135914.js
+++ .history/src/views/Root_20210413135914.js
@@ -17,9 +17,7 @@ const Root = () => (
   <Router>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <TodoContext.Provider
-        value={{ todos: [], deleteTodo: () => {}, addTodo: () => {} }}
-      >
+      <TodoContext.Provider value={{ todos, deleteTodo, addTodo }}>
         <Wrapper>
           <Switch>
             <Route path="/Form">
